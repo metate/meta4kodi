85e19822c0d5ee163df7d25d5f5c64cd3ea82a52
OpenELEQ <OpenELEQ@gmail.com>
2016-04-14 09:13:29 +0200

    - Put movies in seperate folder when adding to library - Changed scraper setting so foldernames get replaced with library titles instead of strmfilenames - Added thumbnail and allowsharing-tag to auto-generated video sources

--- plugin.video.meta/resources/lib/meta/library/tvshows.py
+++ plugin.video.meta/resources/lib/meta/library/tvshows.py
@@ -198,11 +198,12 @@ def setup_library(library_folder):
         # auto configure folder
         msg = _("Would you like to automatically set Meta as a tv shows source?")
         if dialogs.yesno(_("Library setup"), msg):
+            source_thumbnail = "special://home/addons/plugin.video.meta/resources/img/tv.png"
             source_name = "Meta TVShows"
             
             source_content = "('{0}','tvshows','metadata.tvdb.com','',0,0,'<settings><setting id=\"RatingS\" value=\"TheTVDB\" /><setting id=\"absolutenumber\" value=\"false\" /><setting id=\"dvdorder\" value=\"false\" /><setting id=\"fallback\" value=\"true\" /><setting id=\"fanart\" value=\"true\" /><setting id=\"language\" value=\"{1}\" /></settings>',0,0,NULL,NULL)".format(library_folder, LANG)
 
-            add_source(source_name, library_folder, source_content)
+            add_source(source_name, library_folder, source_content, source_thumbnail)
 
     # return translated path
     return xbmc.translatePath(library_folder)
